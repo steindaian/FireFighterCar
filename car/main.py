0861130529a48413b0fc4ea2aced2ba5444cb09f
steindaian <steindaian96@gmail.com>
2018-05-06 15:22:30 +0300

    Socket bug fixed

--- car/main.py
+++ car/main.py
@@ -3,37 +3,115 @@ import socket
 import sys
 import json
 import time
-from thread import Thread
 from motor import Motor
-from camera import Camera
-from stream import MyStream
+#from sensor import Sensor
+import subprocess
+from threading import Thread
 
 def main(server_address):
+    
     car = Motor()
-    car_thread = Thread(target=car.run)
+    print('Car motors system started')
+    #sensor = Sensor()
+    #sensor_thread = Thread(target = sensor.run)
 
-    my_camera = Camera()
-    camera_thread = Thread(target=my_camera.run)
+    start_camera = "sudo systemctl start motion.service"
+    stop_camera = "sudo systemctl stop motion.service"
 
-    my_stream = MyStream(server_adress,car)
-    stream_thread = Thread(target=my_stream.run)
+    connection = None
 
     try:
-        car_thread.start()
-        camera_thread.start()
-        stream_thread.start()
+        p = subprocess.call(start_camera.split())
+        print('Camera feed started')
+        
+        #sensor_thread.start()
+        print("Sensor system started")
+
+        print('Socket communication started')
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(server_address)
+        sock.listen(1)
+
         while True:
-            time.sleep(1)
+            print("Listening for client on port {}".format(server_address[1]))
+            connection, client_address = sock.accept()
+            en = True
+            try:
+                print ('{} connected to {}'.format(time.strftime("%H:%M:%S"), client_address))
+                while en:
+                    buff = b''
+                    while True:
+                        # receive data in chunks
+                        data = connection.recv(1)
+                        if data == b'':
+                            en = False
+                            break
+                        if data != b'\n':
+                            #print (repr(data))
+                            buff += data
+                        else:
+                            #en = False
+                            break
+                    
+                    #print("daian\n")
+                    #buff = str(buff).strip('\n')
+                    if buff == b'':
+                        continue
+                    print (repr(buff))
+                    # parse received data
+                    try:
+                        
+                        json_recv = json.loads(buff.decode('utf-8'))
+                        operation = json_recv['operation']
+                        if operation == "set_speed":
+                            speed = json_recv['speed']
+                            direction = json_recv['direction']
+                            car.setSpeed(direction,speed)
+
+                        elif operation == "set_steering":
+                            direction = json_recv['direction']
+                            steering = json_recv['steering']
+                            car.setSteering(direction,steering)
+                        else :
+                            print(json.dumps(json_recv, indent=4, sort_keys=True))
+
+                    except Exception as e:
+                        print('Something went wrong when running(json didnt decode properly.Disconnecting...')
+                        car.brake()
+                        en = True
+                        pass
+                    finally:
+                        pass
+                        #en = False
+            
+            finally:
+                print ('{} disconnected from {}'.format(time.strftime("%H:%M:%S"), client_address))
+                connection.close()
+                car.brake()
+                en = True
+                pass
 
     except Exception as e:
+        print('Exception occured in starting functionalities')
+        connection.close()
         pass
     finally:
         print('Stopping the car')
         car.stop()
-        car_thread.join()
-        camera_thread.join()
-        stream_thread.join()
+        print('Stopping the socket communication')
+        
+        print('Stopping the camera feed')
+        q = subprocess.call(stop_camera.split())
+        print("Stopping sensor system")
+        #sensor.stop()
+        #sensor_thread.join()
 
 if __name__ == '__main__':
-    server_address = ('localhost', 1221)
-    main(server_address)
+    server_address = ('0.0.0.0', 8089)
+    try:
+        main(server_address)
+    except KeyboardInterrupt as e:
+        raise
+    finally:
+        pass
+    
